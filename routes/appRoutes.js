3a5e358598379494e8ee8877caf9a71c5e250beb
Osama Azab <gosamab@hotmail.com>
2024-05-18 21:16:01 +0300

    add register validations

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,12 +2,21 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middleware/auth");
 const appController = require("../controller/appController");
+const { check } = require("express-validator");
 
 router.get("/", authMiddleware.isAuth, appController.open_index);
 router.get("/login", authMiddleware.isLogged, appController.open_login);
 router.post("/api/login", appController.post_login);
 router.get("/register", authMiddleware.isLogged, appController.open_register);
-router.post("/api/register", appController.post_register);
+router.post(
+  "/api/register",
+  [
+    check("name").notEmpty().isLength({ min: 5, max: 50 }),
+    check("email").notEmpty().isLength({ min: 5, max: 150 }).isEmail(),
+    check("password").notEmpty().isLength({ min: 5, max: 50 }),
+  ],
+  appController.post_register
+);
 router.get("/logout", appController.get_logout);
 
 module.exports = router;
